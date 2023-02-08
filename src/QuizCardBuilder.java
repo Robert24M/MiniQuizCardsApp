4cb969ebcdcf1ec9853da627c7f83592b1210a7d
Robert <mirob1179@gmail.com>
2023-02-08 15:05:11 +0200

    Basic structure of the project

---
+++ src/QuizCardBuilder.java
@@ -0,0 +1,16 @@
+public class QuizCardBuilder {
+    public static void main(String[] args) {
+
+    }
+
+    public void go() {
+        //Build and display GUI
+        //Need a listener for NextCard button
+        //Need a listener for Save button
+        //A listener for New button
+    }
+
+    private void saveFile() {
+        //Handel the saving
+    }
+}

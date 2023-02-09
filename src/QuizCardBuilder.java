d22ea9f5f54e2b04522bb99787a8b991390c1a57
Robert <mirob1179@gmail.com>
2023-02-09 15:20:44 +0200

    Implement the logic

--- src/QuizCardBuilder.java
+++ src/QuizCardBuilder.java
@@ -1,7 +1,9 @@
-import com.sun.deploy.panel.WinUpdatePanel;
-
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -10,9 +12,16 @@ import java.util.function.Supplier;
 public class QuizCardBuilder {
 
     JFrame frame;
-    private JTextArea scrollPane;     //hold the question
+    private JTextArea question;     //hold the question
     private JTextArea answer;       //hold answer
     private List<QuizCard> cardList;
+
+    private static final File dir;
+
+    static {
+        dir = new File("C:\\Users\\micur\\Documents\\QuizCards");
+        System.out.println(dir.mkdir());
+    }
 
     public static void main(String[] args) {
         QuizCardBuilder quizCardBuilder = new QuizCardBuilder();
@@ -40,14 +49,20 @@ public class QuizCardBuilder {
         };
 
         JLabel questionLabel = new JLabel("Question");
-        JTextArea question = textAreaSupplier.get();
+        question = textAreaSupplier.get();
         JScrollPane questionScroller = scrollPaneFunction.apply(question);
 
         JLabel answerLabel = new JLabel("Answer");
-        JTextArea answer = textAreaSupplier.get();
+        answer = textAreaSupplier.get();
         JScrollPane answerScroller = scrollPaneFunction.apply(answer);
 
         JButton nextButton = new JButton("Next Card");
+        nextButton.addActionListener(action -> {
+            System.out.println("next pressed");
+            cardList.add(new QuizCard(question.getText(), answer.getText()));
+            clearCard();
+        });
+
         cardList = new ArrayList<>();
 
         panel.add(questionLabel);
@@ -58,8 +73,26 @@ public class QuizCardBuilder {
 
         JMenuBar menuBar = new JMenuBar();
         JMenu fileMenu = new JMenu("File");
-        JMenuItem newMenuItem = new JMenu("New");
-        JMenuItem saveMenuItem = new JMenu("Save");
+
+        JMenuItem newMenuItem = new JMenuItem("New");
+        newMenuItem.addActionListener(action -> {
+            System.out.println("New pressed");
+            cardList.clear();
+            clearCard();
+        });
+
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        saveMenuItem.addActionListener(action -> {
+            System.out.println("Pressed");
+            cardList.add(new QuizCard(question.getText(), answer.getText()));
+
+            JFileChooser fileSave = new JFileChooser();
+            fileSave.setDialogTitle("File to save");
+            fileSave.setCurrentDirectory(dir);
+            fileSave.showSaveDialog(frame);
+            saveFile(fileSave.getSelectedFile());
+            clearCard();
+        });
 
         fileMenu.add(newMenuItem);
         fileMenu.add(saveMenuItem);
@@ -76,7 +109,19 @@ public class QuizCardBuilder {
         //A listener for New button
     }
 
-    private void saveFile() {
-        //Handel the saving
+    private void saveFile(File file) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (QuizCard card : cardList) {
+                writer.write(card.getQuestion() + "," + card.getAnswer() + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private void clearCard() {
+        question.setText("");
+        answer.setText("");
+        question.requestFocus();
     }
 }
